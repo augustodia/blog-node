2ae060e0f6d01518a26b8a9fdfaa9a5bdb75a1ef
Luiz Augusto <augustodia97@gmail.com>
2023-08-29 09:33:49 -0300

    🔥 remove unnecessary codes

--- back/knexfile.ts
+++ back/knexfile.ts
@@ -21,39 +21,6 @@ const config: { [key: string]: Knex.Config } = {
       loadExtensions: ['.js','.ts']
     }
   },
-
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  }
-
 };
 
 module.exports = config;
