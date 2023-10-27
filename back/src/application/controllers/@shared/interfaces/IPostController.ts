1087128e25142e3ad1c1df38274dbc10238f75cb
Luiz Augusto <augustodia97@gmail.com>
2023-10-27 09:02:41 -0300

    ✨ implements get posts by id and user

--- back/src/application/controllers/@shared/interfaces/IPostController.ts
+++ back/src/application/controllers/@shared/interfaces/IPostController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 
 export default abstract class IUserController {
   abstract getAll(req: Request, res: Response): Promise<Response>;
+  abstract getById(req: Request, res: Response): Promise<Response>;
+  abstract getByUser(req: Request, res: Response): Promise<Response>;
   abstract create(req: Request, res: Response): Promise<Response>;
   abstract update(req: Request, res: Response): Promise<Response>;
   abstract inactivate(req: Request, res: Response): Promise<Response>;
