ef84239a6f713fab71fa9c62834565b14b0d8424
Luiz Santos <56003422+augustodia@users.noreply.github.com>
2024-05-17 15:17:01 -0300

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,7 +3,6 @@ import {defineConfig, normalizePath} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  server: { https: true },
   resolve: {
     alias: {
        '@': normalizePath(resolve(__dirname, './src'))
